263bedcf6ad48ebbab01deca74fff5882e778adf
samuel-tobi <samueltobi52@gmail.com>
2022-06-09 00:15:02 +0100

    functionality updated

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const addInput = document.getElementById("todo-input");
 
 function createTodo() {
   let text = addInput.value;
-
   if (text === "") {
     return;
   }
@@ -31,7 +30,82 @@ function createTodo() {
   addInput.value = "";
 }
 
-addBtn.addEventListener("click", createTodo);
+function showEditInput(paragraphElement) {
+  let editInput = document.getElementsByName("editInput")[0];
+  if (editInput) {
+    editInput.remove();
+  }
+
+  let input = document.createElement("input");
+  input.type = "text";
+  input.name = "editInput";
+  input.value = paragraphElement.textContent;
+  input.classList.add("editInput");
+
+  paragraphElement.paragraphElement.appendChild(input);
+  input.focus();
+}
+
+function updateTodo() {
+  let editInput = document.getElementsByName("editInput")[0];
+  if (!editInput) {
+    return;
+  }
+
+  let newText = editInput.value;
+  if (newText !== "") {
+    let paragraph = editInput.paragraphElement.querySelector(".paragraph");
+    paragraph.textContent = newText;
+  }
+  editInput.remove();
+}
+
+function removeTodo(removeElement) {
+  removeElement.parentElement.remove();
+}
+
+function toggleComplete(inputElement) {
+  if (inputElement.checked === false) {
+    inputElement.parentElement.classList.remove("complete");
+  } else {
+    inputElement.parentElement.classList.add("complete");
+  }
+}
+
+list.addEventListener("click", function (event) {
+  event.stopPropagation();
+  switch (event.target.tagName) {
+    case "P":
+      showEditInput(event.target);
+      break;
+    case "SPAN":
+      removeTodo(event.target);
+      break;
+  }
+});
+
+list.addEventListener("change", function (event) {
+  if (event.target.tagName === "INPUT" && event.target.type === "checkbox") {
+    toggleComplete(event.target);
+  }
+});
+
+list.addEventListener("keypress", function (event) {
+  if (
+    event.target.tagName === "INPUT" &&
+    event.target.type === "text" &&
+    event.key === "Enter"
+  ) {
+    updateTodo();
+  }
+});
+
+document.addEventListener("click", updateTodo);
+
+addBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
+  createTodo();
+});
 addInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     createTodo();
